3b286951c330b646781f4effd9ce574883c66277
Ekam118 <ekamekam449@gmail.com>
2026-07-26 08:49:27 -0600

    fix mail port configuration

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ class Config:
 
     MAIL_SERVER = os.getenv("MAIL_SERVER")
 
-    MAIL_PORT = os.getenv("MAIL_PORT")
+    MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
 
     MAIL_USE_TLS = os.getenv("MAIL_USE_TLS") == "True"
 
