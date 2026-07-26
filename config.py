7ce2febd547353f7e4cdce04a320b7b2f9f5cca2
Ekam118 <ekamekam449@gmail.com>
2026-07-26 11:15:04 -0600

    change gmail smtp to ssl for render

--- config.py
+++ config.py
@@ -26,9 +26,11 @@ class Config:
 
     MAIL_SERVER = os.getenv("MAIL_SERVER")
 
-    MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
+    MAIL_PORT = int(os.getenv("MAIL_PORT", 465))
 
     MAIL_USE_TLS = os.getenv("MAIL_USE_TLS") == "True"
+
+    MAIL_USE_SSL = os.getenv("MAIL_USE_SSL") == "True"
 
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
 
